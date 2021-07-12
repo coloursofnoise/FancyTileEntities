abaaaaa1939c53601ae0de61b2c268f6bc15ccf5
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2021-07-12 16:37:20 -0700

    Extract demo map to separate archive

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 dotnet build
 del fancytileentities.zip
-"C:\Program Files\7-Zip\7z.exe" a fancytileentities.zip Ahorn Dialog Maps FancyTileEntities\bin everest.yaml
+"C:\Program Files\7-Zip\7z.exe" a fancytileentities.zip Ahorn FancyTileEntities\bin everest.yaml
 pause
